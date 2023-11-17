920372c73c6863824fead5b5d16d6762495e1893
oaki-takanoshin <150101357+oaki-takanoshin@users.noreply.github.com>
2023-11-17 15:59:47 +0900

    テスト：はろーわーるど

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package test2;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String msg = "Hello, World!";
+
+        // 標準出力ストリームを経由したコンソール表示
+        System.out.println(msg);
+    }
+}
